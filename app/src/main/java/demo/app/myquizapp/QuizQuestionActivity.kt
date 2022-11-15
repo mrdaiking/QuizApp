0f7996f2014c1346cfd8bdbb155e54e51461139d
cuong <cuong@blay.co.jp>
2022-11-16 07:27:30 +0900

    connect ui

--- app/src/main/java/demo/app/myquizapp/QuizQuestionActivity.kt
+++ app/src/main/java/demo/app/myquizapp/QuizQuestionActivity.kt
@@ -3,8 +3,28 @@ package demo.app.myquizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionActivity : AppCompatActivity() {
+    //create variables for each view in the layout
+    private var progressBar: ProgressBar?=null
+    private var tvProgress: TextView? = null
+    private var tvQuestion:TextView? = null
+    private var ivImage: ImageView? = null
+    private var tvOptionOne:TextView? = null
+    private var tvOptionTwo:TextView? = null
+    private var tvOptionThree:TextView? = null
+    private var tvOptionFour: TextView? = null
+    private var buttonSubmit: Button? = null
+
+    private var mCurrentPosition: Int = 1 // Default and the first question position
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mCorrectAnswers: Int = 0
+    private var mUserName: String? = null
+    private var mSelectedOptionPosition: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
@@ -14,6 +34,18 @@ class QuizQuestionActivity : AppCompatActivity() {
             Log.e("Questions", i.question)
         }
         // END
+        //connect to the view by its id
+        progressBar=findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+        buttonSubmit = findViewById(R.id.btn_submit)
+        mQuestionsList = Constants.getQuestions()
+
     }
 
 
